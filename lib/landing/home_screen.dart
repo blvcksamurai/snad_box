b180af8fc222cc8eb5d5846f21643c4d5d0cfa76
blvcksamurai <theblvcksamurai@gmail.com>
2025-01-05 20:50:29 +0100

    Landing Page(Categories done.)

--- lib/landing/home_screen.dart
+++ lib/landing/home_screen.dart
@@ -150,7 +150,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 CategoriesItem(),
-                                // CategoriesItem(),
+                                CategoriesItem(),
                                 CategoriesItem(),
                               ],
                             ),
